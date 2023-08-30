ea56723c2b3412e9f109ee9ab4cc1964ef75f452
haalahibrahim03 <mdl19ebe025@mec.ac.in>
2023-08-30 09:54:27 +0530

    Commiting to deploy

---
+++ src/Data/PersonalData.js
@@ -0,0 +1,12 @@
+const PersonalData = {
+    firstName: "Abhishek",
+    lastName: "Kumar",
+    nickName:"<  killer - abhi >",
+    mobNo:"+91 8299069282",
+    email:"abhi09148299@gmail.com",
+    address:"Farrukhabad , U.P , India",
+    aboutMe:"Hi Everyone, I am Abhishek Kumar from Uttar Pradesh, India.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB and Firebase. Apart from Coding I love to play games.",
+    resumeLink:'#'
+}
+export const autoTypeData=['2nd Year IT Grad', 'Developer', 'MERN Stack Developer', 'Open Source Contributor'];
+export default PersonalData;
